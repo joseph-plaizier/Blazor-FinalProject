0882e15659c77507d98c85f2221159e18f8d0dc4
Joseph Plaizier <161767237+joseph-plaizier@users.noreply.github.com>
2024-09-23 14:28:16 -0600

    Change project namespace to remove dashes

--- Code/Blazor-SpecialProject-Final/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace Blazor_SpecialProject_Final.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
---
+++ Code/BlazorSpecialProjectFinal/Data/InventoryContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace BlazorSpecialProjectFinal.Data
+{
+    public class InventoryContext : DbContext
+{
+}
+}
