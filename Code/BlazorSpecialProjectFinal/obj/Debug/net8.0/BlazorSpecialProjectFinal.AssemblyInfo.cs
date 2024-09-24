d77f0168418e0c376cb64dfc27456d4b72126e3a
Joseph Plaizier <161767237+joseph-plaizier@users.noreply.github.com>
2024-09-24 16:58:14 -0600

    Add First and Last Name to Registry Page

--- Code/BlazorSpecialProjectFinal/obj/Debug/net8.0/BlazorSpecialProjectFinal.AssemblyInfo.cs
+++ Code/BlazorSpecialProjectFinal/obj/Debug/net8.0/BlazorSpecialProjectFinal.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorSpecialProjectFinal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5032ac357038b4c4b0742fa13aad6b30e2d7a9d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf6b726cd06535c4df909fe65df9c6da1318bf08")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorSpecialProjectFinal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorSpecialProjectFinal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
